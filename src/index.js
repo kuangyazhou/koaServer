bdae2dae19074967aef3ad29d35522917378ac1d
kuangyazhou <kuangyazhou@tuandai.com>
2018-07-31 16:51:50 +0800

    fuck too

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import "./index.css";
+// import registerServiceWorker from "./registerServiceWorker";
 import { routeConfig } from "./router";
-
-import "./utils/format";
-import "antd/dist/antd.css";
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <Router>
-      <div style={{ height: "100%" }}>{routeConfig}</div>
-  </Router>,
-  document.getElementById("root")
+    <Router>
+        <div style={{ height: "100%" }}>{routeConfig}</div>
+    </Router>,
+    document.getElementById("root")
 );
