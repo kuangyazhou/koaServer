732cb37dd3cc60dd9fc7e7b78fe677b99049013c
kuangyazhou <kuangyazhou@tuandai.com>
2018-07-31 17:40:20 +0800

    报错

--- src/index.js
+++ src/index.js
@@ -10,14 +10,10 @@ import "antd/dist/antd.css";
 
 ReactDOM.render(
     <Router>
-        <div style={{ height: "100%" }}>
-        {routeConfig}
-        </div>
-  </Router>
+        <div style={{ height: "100%" }}>{routeConfig}</div>
+    </Router>,
     document.getElementById("root")
 );
-
-
 
 // {
 //   "plugins": [
--- src/pages/page1.js
+++ src/pages/page1.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { DatePicker } from "antd";
-import Header from "./header";
+import Header from "../commponents/header";
 
 export default class Page1 extends React.Component {
     render() {
         return (
             <div>
-                <Header></Header>
+                <Header />
                 <h1>this is the page1</h1>
-                <div></div>
+                <div />
                 <div>
                     <DatePicker />
                 </div>
