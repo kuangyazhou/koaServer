4756e3e08542533b4d67442571b67873dbcd0697
kuangyazhou <kuangyazhou@tuandai.com>
2018-07-31 18:13:01 +0800

    less配置

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import "./page2.less";
+
 export default class Page1 extends React.Component {
     render() {
-        return <h1>this is the page2</h1>;
+        return (
+            <div>
+                <h1>this is the page2</h1>
+                <span className="cyan">fuck the king</span>
+            </div>
+        );
     }
 }
