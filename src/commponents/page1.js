f416f7d2b5d6ab99ddb6887121b662303f6cac9e
周瑞雪 <zhrx121314@163.com>
2018-07-31 17:23:51 +0800

    add header

--- src/commponents/page1.js
+++ src/commponents/page1.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { DatePicker } from "antd";
+import Header from "./header";
 
 export default class Page1 extends React.Component {
     render() {
         return (
             <div>
+                <Header></Header>
                 <h1>this is the page1</h1>
+                <div></div>
                 <div>
                     <DatePicker />
                 </div>
