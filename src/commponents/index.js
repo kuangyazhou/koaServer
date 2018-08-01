d79898748a27fa016aff8cdd075602a0e53819e8
周瑞雪 <zhrx121314@163.com>
2018-08-01 14:34:29 +0800

    add jsx alias

--- src/commponents/index.js
+++ src/commponents/index.js
@@ -2,8 +2,4 @@ import Page1 from "./page1";
 import Page2 from "./page2";
 import Page3 from "./page3";
 
-module.exports = {
-    Page1: Page1,
-    Page2: Page2,
-    Page3: Page3
-};
+export default { Page1, Page2, Page3 };
