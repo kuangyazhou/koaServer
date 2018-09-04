6b77078b193280072ca1f64cd2ba3a2e5da9f32b
况亚洲 <18707174713@139.com>
2018-09-04 10:21:56 +0800

    mysql

--- server/api/db.sql
+++ server/api/db.sql
@@ -11,3 +11,5 @@ SELECT a.runoob_id, a.runoob_author, b.runoob_count FROM runoob_tbl a INNER JOIN
 从tcount_tbl表中查找所有与runoob_tbl表中runoob_author相等的runoob_count值;
 
 SElECT * from data;
+
+SELECT name from data;
